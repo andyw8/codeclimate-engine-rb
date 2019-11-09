97075b76b03a9b42d191bb2393a36393b6eef09d
Andy Waite <andyw8@users.noreply.github.com>
2019-11-09 15:55:21 -0500

    Enable strict type checking for Config

--- lib/cc_engine/config.rb
+++ lib/cc_engine/config.rb
@@ -1,30 +1,39 @@
-# typed: true
+# typed: strict
 require "json"
 
 module CCEngine
   class Config
+    extend T::Sig
+
+    sig { params(json_string: String).void }
     def initialize(json_string)
-      @json_string = json_string
+      @json_string = T.let(json_string, String)
+      @parsed_json = T.let(nil, T.nilable(T::Hash[String, T.untyped]))
     end
 
+    sig { returns(T::Array[String]) }
     def include_paths
       parsed_json.fetch("include_paths")
     end
 
+    sig { returns(T::Array[String]) }
     def exclude_paths
       parsed_json.fetch("exclude_paths")
     end
 
+    sig { returns(T::Boolean) }
     def enabled?
       parsed_json.fetch("enabled")
     end
 
     protected
 
+    sig { returns(String) }
     attr_reader :json_string
 
     private
 
+    sig {returns(T::Hash[String, T.untyped])}
     def parsed_json
       @parsed_json ||= JSON.parse(json_string)
     end
